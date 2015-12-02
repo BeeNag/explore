727c2252cd270540d8fe8023e28dd3663dc77c03
BeeNag <bnaglerose88@gmail.com>
2015-12-02 19:09:13 +0000

    Adding a hotels controller

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,6 +1,27 @@
+class Hotel
+	attr_accessor :name 
+end
+
 class HotelsController < ApplicationController
 
 	def index
+
+		@hotels = []
+
+		hotel1 = Hotel.new
+		hotel1.name = "The Hilton Park Lane"
+
+		hotel2 = Hotel.new
+		hotel2.name = "The Continental"
+
+		hotel3 = Hotel.new
+		hotel3.name = "The Shangri-La"
+
+		@hotels << hotel1
+		@hotels << hotel2
+		@hotels << hotel3
+
+		# @hotels = ['The Mandarin Oriental', 'The Rosewood', 'The Dorchester', 'Grosvenor House']
 	end
-	
+
 end
