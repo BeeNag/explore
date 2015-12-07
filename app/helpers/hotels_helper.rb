577e00861a8398347780ae6a98d84e9cdf749ff6
BeeNag <bnaglerose88@gmail.com>
2015-12-07 20:45:35 +0000

    Update to explore project

--- app/helpers/hotels_helper.rb
+++ app/helpers/hotels_helper.rb
@@ -1,2 +1,18 @@
 module HotelsHelper
+	 def format_body(hotel)
+        if hotel.body.present?
+            # note you can call a helper from a helper
+            truncate(hotel.body, length: 20, separator: ' ')
+        else
+            content_tag(:i, 'n/a')
+        end
+    end
+
+    def format_price(hotel)
+    	if hotel.body.present?
+    		hotel.price
+    	else
+    		content_tag(:i, 'On Request')
+    	end
+    end
 end
